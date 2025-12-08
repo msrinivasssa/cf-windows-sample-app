84822ee6b142306020280f190dd7e9025fd7abc4
Madhavan <msrinivasssa@vmware.com>
2025-12-08 13:51:49 +0530

    fixing 500 error

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
 
+// Get port from Cloud Foundry PORT environment variable, default to 8080
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+
 // Add services to the container
 builder.Services.AddControllers();
 
